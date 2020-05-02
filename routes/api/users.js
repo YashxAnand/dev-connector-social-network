073a3f39d5853ee2a7ec1bb9f2e7bdeceaa4ae9a
YashxAnand <48432376+YashxAnand@users.noreply.github.com>
2020-05-03 01:17:31 +0530

    Second commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,44 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../../models/User");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 router.get("/test", (req, res) => res.json({ msg: "User works" }));
+
+router.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        return res.status(400).json({ email: "Email already in use" });
+      } else {
+        const avatar = gravatar.url(email, {
+          s: "200", //Size
+          r: "pg", //Rating
+          d: "mm", //default
+        });
+
+        const user = new User({
+          name,
+          email,
+          password,
+          avatar,
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) throw err;
+            user.password = hash;
+            user
+              .save()
+              .then((user) => res.send(user))
+              .catch((err) => conso.log(err));
+          });
+        });
+      }
+    })
+    .catch((err) => console.log(err.message));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const bodyParser = require("body-parser");
+
+//Create a body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Initializing routes
 const users = require("./routes/api/users");
